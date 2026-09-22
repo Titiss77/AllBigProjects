9a533e24e05b35755c7408a0a73ff823331909b5
Titiss77 <Mathis.fcs.lav@gmail.com>
2026-09-22 17:46:11 +0200

    return

--- titiss.space/htdocs/app/Config/Filters.php
+++ titiss.space/htdocs/app/Config/Filters.php
@@ -38,7 +38,7 @@ class Filters extends BaseFilters
 
     public array $required = [
         'before' => [
-            // 'forcehttps', <-- COMMENTEZ CETTE LIGNE
+            'forcehttps', // Géré via .htaccess maintenant
             'pagecache',
         ],
         'after' => [
