fd07b72a4b79874a369f8cc3e7ef9ec863a81943
Titiss77 <Mathis.fcs.lav@gmail.com>
2026-09-24 23:29:32 +0200

    good

--- titiss.space/htdocs/app/Controllers/ProfileController.php
+++ titiss.space/htdocs/app/Controllers/ProfileController.php
@@ -26,15 +26,23 @@ class ProfileController extends BaseController
         $statusTermine = $itemModel->where('id_user', $user->id)->where('status', 'Terminé')->countAllResults();
         $statusAucun = $itemModel->where('id_user', $user->id)->where('status', 'Aucun')->countAllResults();
 
+        $episodesStats = $itemModel->getGlobalEpisodesStats($user->id);
+        $inProgressSeries = $itemModel->getInProgressSeriesStats($user->id);
+
         $data = [
-            'user' => $user,
-            'totalItems' => $totalItems,
-            'publicItems' => $publicItems,
-            'statusAVoir' => $statusAVoir,
+            'user'          => $user,
+            'totalItems'    => $totalItems,
+            'publicItems'   => $publicItems,
+            'statusAVoir'   => $statusAVoir,
             'statusEnCours' => $statusEnCours,
             'statusEnPause' => $statusEnPause,
             'statusTermine' => $statusTermine,
-            'statusAucun' => $statusAucun,
+            'statusAucun'   => $statusAucun,
+
+            // Nouvelles variables injectées vers la vue
+            'totalVus'         => $episodesStats->total_vus ?? 0,
+            'totalRestants'    => $episodesStats->total_restants ?? 0,
+            'inProgressSeries' => $inProgressSeries,
         ];
 
         return view('profile/index', $data);
